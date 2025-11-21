743c512ed928709a0046ce880bf7111d58b07471
mirko <mirkofebbo@gmail.com>
2025-11-21 16:44:05 +0000

    add auth to get the station status

--- lockncharge_api/api.py
+++ lockncharge_api/api.py
@@ -1,9 +1,27 @@
 import requests
+from lockncharge_api.auth import LocknChargeAuth
 
 class LocknChargeAPI:
-    def __init__(self, api_url: str):
+    def __init__(self, api_url: str, station_id:str, client_id:str, client_secret:str):
         print('Started API')
-        self.url:str = api_url
-        self.token:str = None
-    
+        self.url:str                = api_url
+        self.id:str                 = station_id
+        self.auth:LocknChargeAuth   = LocknChargeAuth(api_url, client_id, client_secret)
+        self.token:str              = self.auth.get_token()
 
+
+    def get_connection_status(self):
+        # we can get alot of info from this call: id, name connected, firmwareVersion, update, lockdown, tags, nodeId
+        # But we are only interested to know if the locker is connected.
+        url:str = f'{self.url}stations/{self.id}'
+        headers:dict = {
+            'Accept': 'application/json',
+            'Authorization': f"Bearer {self.token}"
+        }
+
+        response:requests.Response = requests.get(url, headers=headers)
+        data:dict = response.json()
+        
+        return data["connected"]
+
+ 
