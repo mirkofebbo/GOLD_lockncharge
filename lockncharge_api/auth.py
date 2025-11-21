77fec4b58c79993a3fff89d5bd33912fdaf95e1b
mirko <mirkofebbo@gmail.com>
2025-11-21 15:48:15 +0000

    objectify auth functions

---
+++ lockncharge_api/auth.py
@@ -0,0 +1,68 @@
+import requests
+import time
+import json
+
+class LocknChargeAuth:
+    def __init__(self, api_url: str, client_id: str, client_secret: str):
+        
+        self.url:str = api_url
+        self.id:str  = client_id
+        self.secret:str  = client_secret
+
+        self.token_data:dict = self.load_local_token()
+
+        if not self.check_token_expiry():
+            self.token_data:dict = self.api_get_token()
+            self.save_token()
+
+    def load_local_token(self):
+        file_path:str = "./data/token.json"
+
+        try:
+            # Check if the file exists + load
+            with open(file_path, 'r') as file:
+                data:dict = json.load(file)
+        except FileNotFoundError:
+            # else create an empty file
+            data:dict = {"access_token": None, "expires": None}
+            with open(file_path, 'w') as file:
+                json.dump(data, file)
+
+        return data
+
+    def check_token_expiry(self):
+        if self.token_data['access_token'] is None:
+            return False
+        elif self.token_data['expires'] < time.time():
+            return False
+        else:
+            return True
+        
+    def save_token(self):
+        file_path:str = "data/token.json"
+
+        with open(file_path, 'w') as file:
+            json.dump(self.token_data, file)
+
+
+    def api_get_token(self):  
+        url:str = self.url + "token"
+        data:dict = {
+            "client_id": self.id,
+            "client_secret": self.secret
+        }
+
+        response:requests.Response = requests.post(url, data=data)
+
+        token:dict = response.json()
+        return token
+    
+    def get_token(self):
+
+        if not self.check_token_expiry():
+            print("Token expired or not found, fetching new token...")
+            self.token_data = self.get_token()
+            self.save_token()
+        
+        return self.token_data["access_token"]
+
