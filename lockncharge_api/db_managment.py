893423d580f6c48ab8f8f58f367b656a09b5a864
mirko <mirkofebbo@gmail.com>
2025-12-05 15:02:09 +0000

    adding entry to DB

--- lockncharge_api/db_managment.py
+++ lockncharge_api/db_managment.py
@@ -11,15 +11,17 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
-
 # conn = sqlite3.connect('./data/example.db')  # Creates a new database file if it doesn’t exist
 # cursor = conn.cursor()
-# cursor.execute("CREATE TABLE RHB115(username TEXT, user_id TEXT, bay_number INTEGER, assigned_time_utc REAL, assigned_time_human DATETIME, returned_time_utc REAL, returned_time_human DATETIME)")
+# cursor.execute(
+#     "CREATE TABLE IF NOT EXISTS RHB115 ("
+#     "user_id TEXT, username TEXT, bay_number INTEGER, book_timestamp TEXT, return_timestamp TEXT)"
+# )
 
-# # # add dummy data
-# # cursor.execute("INSERT INTO RHB115 (username, user_id, bay_number, assigned_time_utc, assigned_time_human, returned_time_utc, returned_time_human) VALUES (?, ?, ?, ?, ?, ?, ?)", ("testuser", "user123", 1, 1622548800.0, "2021-06-01 12:00:00", None, None))
-# # conn.commit()
-# # conn.close()
+# # add dummy data
+# cursor.execute("INSERT INTO RHB115 (user_id, username, bay_number, book_timestamp, return_timestamp) VALUES (?, ?, ?, ?, ?)", ("3e410120-6c9a-40b1-a16c-69a15cb9e85e", "temp", 1, "2021-06-01 12:00:00", None))
+# conn.commit()
+# conn.close()
 
 class DatabaseManager:
     def __init__(self, db_path: str):
@@ -30,7 +32,10 @@ class DatabaseManager:
         try:
             conn = sqlite3.connect(self.db_path)
             cursor = conn.cursor()
-            cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({schema})")
+            cursor.execute(
+                f"CREATE TABLE IF NOT EXISTS {table_name} ("
+                "user_id TEXT, username TEXT, bay_number INTEGER, book_timestamp TEXT, return_timestamp TEXT)"
+            )
             conn.commit()
             conn.close()
             logger.info(f"[DB] Table '{table_name}' created or already exists.")
@@ -51,11 +56,23 @@ class DatabaseManager:
             return None
         
     # ==== DATA PARSING METHODS =====
+    def new_data_parsing(self, data:dict):
+        formated_data = {
+            "user_id": data["data"]["user"]["id"],
+            "username": data["data"]["user"].get("name", data["userType"]) ,
+            "bay_number": data["data"]["bay"],
+            "book_timestamp": data["timestamp"],
+            "return_timestamp": ""
+        }
+        return formated_data
+    
     def add_entry(self, table_name: str, data: dict):  
-        print(f"[db_managment]: {data}")
-        keys = ', '.join(data.keys())
-        question_marks = ', '.join(['?'] * len(data))
-        values = tuple(data.values())
+
+        parsed_data:dict = self.new_data_parsing(data)
+        print(f"[db_managment]: {parsed_data}")
+        keys = ', '.join(parsed_data.keys())
+        question_marks = ', '.join(['?'] * len(parsed_data))
+        values = tuple(parsed_data.values())
         query = f"INSERT INTO {table_name} ({keys}) VALUES ({question_marks})"
         self.execute_query(query, values)
-        logger.info(f"[DB] Entry added to '{table_name}': {data}")
+        logger.info(f"[DB] Entry added to '{table_name}': {parsed_data}")
